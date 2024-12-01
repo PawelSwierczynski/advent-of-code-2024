d41dda5f6a0e24fe27e75844d163dd93412e6cde
Paweł Świerczyński <swierczynski.pawel.karol@gmail.com>
2024-12-01 21:13:58 +0100

    Rename the main window

--- AdventOfCode/AdventOfCodeWindow.xaml.cs
+++ AdventOfCode/AdventOfCodeWindow.xaml.cs
@@ -14,9 +14,9 @@ namespace AdventOfCode
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdcentOfCodeWindow : Window
     {
-        public MainWindow()
+        public AdcentOfCodeWindow()
         {
             InitializeComponent();
         }
